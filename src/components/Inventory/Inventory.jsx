3ee07c192ca3cccace9e431b401d3bc614b02028
Uzzal Bhowmik <ubcreativity111@gmail.com>
2023-09-17 20:08:56 +0600

    project finalized

--- src/components/Inventory/Inventory.jsx
+++ src/components/Inventory/Inventory.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import Product from "../Product/Product";
 
 const Inventory = () => {
-    return (
-        <div>
-            <h3>Inventory page</h3>
-        </div>
-    );
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:5000/products`)
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+  return (
+    <div>
+      <h2 style={{ marginLeft: "20px" }}>
+        Available Products In The Inventory
+      </h2>
+      <div
+        style={{
+          width: "90%",
+          marginTop: "50px",
+          marginInline: "auto",
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr 1fr 1fr",
+          gridRowGap: "40px",
+        }}
+      >
+        {products.map((product) => (
+          <Product key={product._id} product={product}></Product>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Inventory;
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,89 +1,143 @@
-import React, { useEffect, useState } from 'react';
-import { addToDb, deleteShoppingCart, getShoppingCart } from '../../utilities/fakedb';
-import Cart from '../Cart/Cart';
-import Product from '../Product/Product';
-import './Shop.css';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import {
+  addToDb,
+  deleteShoppingCart,
+  getShoppingCart,
+} from "../../utilities/fakedb";
+import Cart from "../Cart/Cart";
+import Product from "../Product/Product";
+import "./Shop.css";
+import { Link, useLoaderData } from "react-router-dom";
 
 const Shop = () => {
-    const [products, setProducts] = useState([]);
-    const [cart, setCart] = useState([])
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [productsPerPage, setProductsPerPage] = useState(5);
 
-    useEffect(() => {
-        fetch('products.json')
-            .then(res => res.json())
-            .then(data => setProducts(data))
-    }, []);
+  useEffect(() => {
+    fetch(
+      `http://localhost:5000/products?page=${currentPage}&limit=${productsPerPage}`
+    )
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, [currentPage, productsPerPage]);
 
-    useEffect(() => {
-        const storedCart = getShoppingCart();
+  useEffect(() => {
+    const storedCart = getShoppingCart();
+    const cartProductIds = Object.keys(storedCart);
+
+    fetch("http://localhost:5000/productsById", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(cartProductIds),
+    })
+      .then((res) => res.json())
+      .then((data) => {
         const savedCart = [];
         // step 1: get id of the addedProduct
         for (const id in storedCart) {
-            // step 2: get product from products state by using id
-            const addedProduct = products.find(product => product.id === id)
-            if (addedProduct) {
-                // step 3: add quantity
-                const quantity = storedCart[id];
-                addedProduct.quantity = quantity;
-                // step 4: add the added product to the saved cart
-                savedCart.push(addedProduct);
-            }
-            // console.log('added Product', addedProduct)
+          // step 2: get product from products state by using id
+          const addedProduct = data.find((product) => product._id === id);
+          if (addedProduct) {
+            // step 3: add quantity
+            const quantity = storedCart[id];
+            addedProduct.quantity = quantity;
+            // step 4: add the added product to the saved cart
+            savedCart.push(addedProduct);
+          }
+          // console.log('added Product', addedProduct)
         }
         // step 5: set the cart
         setCart(savedCart);
-    }, [products])
+      });
+  }, []);
 
-    const handleAddToCart = (product) => {
-        // cart.push(product); '
-        let newCart = [];
-        // const newCart = [...cart, product];
-        // if product doesn't exist in the cart, then set quantity = 1
-        // if exist update quantity by 1
-        const exists = cart.find(pd => pd.id === product.id);
-        if (!exists) {
-            product.quantity = 1;
-            newCart = [...cart, product]
-        }
-        else {
-            exists.quantity = exists.quantity + 1;
-            const remaining = cart.filter(pd => pd.id !== product.id);
-            newCart = [...remaining, exists];
-        }
-
-        setCart(newCart);
-        addToDb(product.id)
+  const handleAddToCart = (product) => {
+    // cart.push(product); '
+    let newCart = [];
+    // const newCart = [...cart, product];
+    // if product doesn't exist in the cart, then set quantity = 1
+    // if exist update quantity by 1
+    const exists = cart.find((pd) => pd._id === product._id);
+    if (!exists) {
+      product.quantity = 1;
+      newCart = [...cart, product];
+    } else {
+      exists.quantity = exists.quantity + 1;
+      const remaining = cart.filter((pd) => pd._id !== product._id);
+      newCart = [...remaining, exists];
     }
 
-    const handleClearCart = () => {
-        setCart([]);
-        deleteShoppingCart();
-    }
+    setCart(newCart);
+    addToDb(product._id);
+  };
 
-    return (
-        <div className='shop-container'>
-            <div className="products-container">
-                {
-                    products.map(product => <Product
-                        key={product.id}
-                        product={product}
-                        handleAddToCart={handleAddToCart}
-                    ></Product>)
-                }
-            </div>
-            <div className="cart-container">
-                <Cart
-                    cart={cart}
-                    handleClearCart={handleClearCart}
-                >
-                    <Link className='proceed-link' to="/orders">
-                        <button className='btn-proceed'>Review Order</button>
-                    </Link>
-                </Cart>
-            </div>
+  const handleClearCart = () => {
+    setCart([]);
+    deleteShoppingCart();
+  };
+
+  // pagination
+  const { totalProducts } = useLoaderData();
+
+  const pageNeeded = Math.ceil(totalProducts / productsPerPage);
+
+  const pageNumbers = [...Array(pageNeeded).keys()];
+
+  const options = [5, 10, 15, 20];
+
+  return (
+    <>
+      <div className="shop-container">
+        <div className="products-container">
+          {products.map((product) => (
+            <Product
+              key={product._id}
+              product={product}
+              handleAddToCart={handleAddToCart}
+            ></Product>
+          ))}
         </div>
-    );
+        <div className="cart-container">
+          <Cart cart={cart} handleClearCart={handleClearCart}>
+            <Link className="proceed-link" to="/orders">
+              <button className="btn-proceed">Review Order</button>
+            </Link>
+          </Cart>
+        </div>
+      </div>
+
+      <div className="pagination">
+        {pageNumbers.map((number) => (
+          <button
+            key={number}
+            onClick={() => setCurrentPage(number)}
+            className={`${currentPage === number ? "selected" : ""}`}
+          >
+            {number}
+          </button>
+        ))}
+
+        <select
+          name=""
+          id=""
+          onChange={(e) => {
+            setProductsPerPage(e.target.value);
+            setCurrentPage(0);
+          }}
+        >
+          {options.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      </div>
+    </>
+  );
 };
 
 export default Shop;
